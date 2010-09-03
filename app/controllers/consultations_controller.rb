3db8d2bedd128c60b7ea732f330e823aae2ee0f2
rolo555 <rolo555@gmail.com>
2010-09-03 00:41:52 -0400

    se trato de ordenar la consulta y eliminacino de los views de phone_number

--- app/controllers/consultations_controller.rb
+++ app/controllers/consultations_controller.rb
@@ -1,3 +1,14 @@
 class ConsultationsController < ApplicationController
-  active_scaffold :consultation
+
+  active_scaffold :consultation do |conf|
+    #Configuración de las columnas que se mostrarán al listar
+    conf.list.columns = [:wight, :height, :head_circumference, :anterior_fontanel, :temperature, :current_condition, :diagnosis, :medical_certificate, :recipe, :order, :laboratory, :amount]
+
+    #Configuración de las columnas que se excluiran para todas las acciones
+    conf.columns.exclude :created_at, :updated_at, :patient
+
+    #Configuración de las acciones que se mostrarán
+    conf.actions = [:create, :search, :update, :delete, :show, :nested, :subform, :list]
+
+  end
 end
