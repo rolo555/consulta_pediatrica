c4e69b7fff94f47488d1e2b39ace916cde8e33da
rolo555 <rolo555@gmail.com>
2010-09-03 00:56:23 -0400

    modificacion de la vista de consultation al listar

--- app/controllers/consultations_controller.rb
+++ app/controllers/consultations_controller.rb
@@ -1,8 +1,11 @@
 class ConsultationsController < ApplicationController
 
   active_scaffold :consultation do |conf|
+    #Configuración de las columnas que se mostrarán 
+    conf.columns = [:weight, :height, :head_circumference, :anterior_fontanel, :temperature, :current_condition, :diagnosis, :medical_certificate, :recipe, :order, :laboratory, :amount]
+
     #Configuración de las columnas que se mostrarán al listar
-    conf.list.columns = [:wight, :height, :head_circumference, :anterior_fontanel, :temperature, :current_condition, :diagnosis, :medical_certificate, :recipe, :order, :laboratory, :amount]
+    conf.list.columns = [:created_at, :diagnosis, :recipe, :amount]
 
     #Configuración de las columnas que se excluiran para todas las acciones
     conf.columns.exclude :created_at, :updated_at, :patient
