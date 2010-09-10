3b04603ad45f62943c05849ef6ce960582cb93b1
Jesús Mercado <jesusmmm@gmail.com>
2010-09-09 23:04:02 -0400

    Record_select plugin instalado

---
+++ vendor/plugins/recordselect/test/recordselect_test.rb
@@ -0,0 +1,10 @@
+require 'rubygems'
+require 'test/unit'
+require 'active_support'
+
+class RecordselectTest < ActiveSupport::TestCase
+  # Replace this with your real tests.
+  def test_this_plugin
+    assert true
+  end
+end
