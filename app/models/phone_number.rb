f6e880faa51be0a0b636afa3e4563f7e736a3e12
rolo555 <rolo555@gmail.com>
2010-09-13 01:04:44 -0400

    test del modelo de Phone_number

--- app/models/phone_number.rb
+++ app/models/phone_number.rb
@@ -1,7 +1,13 @@
 class PhoneNumber < ActiveRecord::Base
   belongs_to :patient
+  validates_presence_of :number
 
   def to_label
     number
   end
+
+  def before_validation
+    self.number.strip! if self.number.presence
+  end
+
 end
--- test/unit/phone_number_test.rb
+++ test/unit/phone_number_test.rb
@@ -1,4 +1,30 @@
 require 'test_helper'
 
 class PhoneNumberTest <  Test::Unit::TestCase
+
+  should belong_to(:patient)
+  should validate_presence_of :number
+
+  def test_to_label
+    p_number = PhoneNumber.new(:number => "123")
+    assert_equal(p_number.to_label, "123")
+  end
+
+  def sanitizate(f)
+    eval(
+      "
+      def sanitizate_#{f}
+        p_number = PhoneNumber.new(:number => ' 123 ')
+        p_number.before_validation
+        assert_equal('123', p_number.instance_eval('#{f}'))
+      end
+      "
+    )
+    eval("sanitizate_#{f}")
+  end
+
+  should "sanitizate phone_number" do
+    sanitizate("number")
+  end
+
 end
