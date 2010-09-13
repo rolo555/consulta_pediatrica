7b0cc9cb1ef360cb020f3d295a5160ef29952ba8
rolo555 <rolo555@gmail.com>
2010-09-13 01:34:17 -0400

    test model Place

--- app/models/place.rb
+++ app/models/place.rb
@@ -3,10 +3,16 @@ class Place < ActiveRecord::Base
   validates_presence_of :city, :country
   validates_uniqueness_of :city,
     :scope => [:country],
-    :message => " and Country already exist"
+#    :message => " and Country already exist",
+    :case_sensitive => false
 
   def to_label
     "#{city} #{country}"
+  end
+
+  def before_validation
+    self.city.strip! if self.city.presence
+    self.country.strip! if self.country.presence
   end
   
 end
--- test/unit/place_test.rb
+++ test/unit/place_test.rb
@@ -1,4 +1,35 @@
 require 'test_helper'
 
 class PlaceTest <  Test::Unit::TestCase
+  should have_many(:patients)
+  should validate_presence_of :city
+  should validate_presence_of :country
+  should validate_uniqueness_of(:city).scoped_to(:country).case_insensitive
+
+  def test_to_label
+    place = Place.new(:city => "city", :country => "country")
+    assert_equal(place.to_label, "city country");
+  end
+
+  def sanitizate(f)
+    eval(
+      "
+      def sanitizate_#{f}
+        place = Place.new(:city => ' p ', :country => ' p ')
+        place.before_validation
+        assert_equal('p', place.instance_eval('#{f}'))
+      end
+      "
+    )
+    eval("sanitizate_#{f}")
+  end
+
+  should "sanitizate city" do
+    sanitizate("city")
+  end
+
+  should "sanitizate country" do
+    sanitizate("country")
+  end
+
 end
