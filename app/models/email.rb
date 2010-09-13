33555467de269af9e04853b43c50adceb88f0375
rolo555 <rolo555@gmail.com>
2010-09-13 02:01:23 -0400

    test model Email

--- app/models/email.rb
+++ app/models/email.rb
@@ -1,8 +1,16 @@
 class Email < ActiveRecord::Base
   belongs_to :patient
+  validates_presence_of :address
+
+  validates_format_of :address,
+    :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
 
   def to_label
     address
   end
 
+  def before_validation
+    self.address.strip! if self.address.presence
+  end
+  
 end
--- test/unit/email_test.rb
+++ test/unit/email_test.rb
@@ -1,4 +1,37 @@
 require 'test_helper'
 
 class EmailTest < Test::Unit::TestCase
+  should validate_presence_of :address
+  should belong_to :patient
+
+  should_not allow_value("mail").for(:address)
+  should_not allow_value("mail@").for(:address)
+  should_not allow_value("@mail").for(:address)
+  should_not allow_value("mail.com").for(:address)
+  should_not allow_value("@.com").for(:address)
+  should_not allow_value("mail@com").for(:address)
+
+
+  def test_to_label
+    mail = Email.new(:address => "mail@mail.com")
+    assert_equal(mail.to_label, "mail@mail.com");
+  end
+
+  def sanitizate(f)
+    eval(
+      "
+      def sanitizate_#{f}
+        mail = Email.new(:address => ' mail@mail.com ')
+        mail.before_validation
+        assert_equal('mail@mail.com', mail.instance_eval('#{f}'))
+      end
+      "
+    )
+    eval("sanitizate_#{f}")
+  end
+
+  should "sanitizate address" do
+    sanitizate("address")
+  end
+
 end
