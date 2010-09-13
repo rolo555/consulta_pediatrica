3d340c3c7acaef5ebd4ef3fa6e89e43050cfcbfc
rolo555 <rolo555@gmail.com>
2010-09-13 03:57:39 -0400

    test model consultation_price

--- app/models/consultation_price.rb
+++ app/models/consultation_price.rb
@@ -1,10 +1,14 @@
 class ConsultationPrice < ActiveRecord::Base
   has_many :patients
-  validates_uniqueness_of :price_type
+  validates_uniqueness_of :price_type, :case_sensitive => false
   validates_presence_of :price_type, :amount
 
   def to_label
     "Type of Price: #{price_type}\nAmount: #{amount}"
+  end
+
+    def before_validation
+    self.price_type.strip! if self.price_type.presence
   end
 
 end
--- test/unit/consultation_price_test.rb
+++ test/unit/consultation_price_test.rb
@@ -1,8 +1,18 @@
 require 'test_helper'
 
-class ConsultationPriceTest <  Test::Unit::TestCase
+class ConsultationPriceTest < ActiveSupport::TestCase
   should validate_presence_of :price_type
   should validate_presence_of :amount
   should have_many(:patients)
   should validate_uniqueness_of(:price_type).case_insensitive
+
+  def test_to_label
+    c_price = ConsultationPrice.new(:price_type => "price type", :amount => 1.0 )
+    assert_equal(c_price.to_label, "Type of Price: price type\nAmount: 1.0");
+  end
+
+  should "sanitizate price_type" do
+    sanitizate("price_type")
+  end
+
 end
