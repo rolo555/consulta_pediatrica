91ef5c45244adc28f212236d46c02a3f1dd9341d
rolo555 <rolo555@gmail.com>
2010-09-13 00:44:28 -0400

    test modelo Address

--- app/models/address.rb
+++ app/models/address.rb
@@ -4,5 +4,11 @@ class Address < ActiveRecord::Base
   def to_label
     address
   end
-  
+
+  validates_presence_of :address
+
+  def before_validation
+    self.address.strip! if self.address.presence
+  end
+
 end
--- test/unit/address_test.rb
+++ test/unit/address_test.rb
@@ -1,4 +1,29 @@
 require 'test_helper'
 
 class AddressTest < Test::Unit::TestCase
+  should belong_to(:patient)
+  should validate_presence_of :address
+
+  def test_to_label
+    address = Address.new(:address => "Antezana")
+    assert_equal(address.to_label, "Antezana");
+  end
+
+  def sanitizate(f)
+    eval(
+      "
+      def sanitizate_#{f}
+        ads = Address.new(:address => ' a ')
+        ads.before_validation
+        assert_equal('a', ads.instance_eval('#{f}'))
+      end
+      "
+    )
+    eval("sanitizate_#{f}")
+  end
+
+  should "sanitizate address" do
+    sanitizate("address")
+  end
+
 end
