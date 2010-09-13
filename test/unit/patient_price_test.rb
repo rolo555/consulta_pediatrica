283586c4a21665f84e67b458abdd8f3b5348ef6a
rolo555 <rolo555@gmail.com>
2010-09-13 02:25:02 -0400

    test model patient price no completas!!!!

--- test/unit/patient_price_test.rb
+++ test/unit/patient_price_test.rb
@@ -1,4 +1,10 @@
 require 'test_helper'
 
 class PatientPriceTest <  Test::Unit::TestCase
+  shoudl validate_presence_of :price_type
+  shoudl validate_presence_of :amount
+  should have_may(:patients)
+  should validete_uniqueness_of(:price_type).case_insensitive
+
+
 end
