09583a134d7a7ab3b0bcfe4579cb853c268b4a2e
Jesús Mercado <jesusmmm@gmail.com>
2010-09-21 18:56:56 -0400

    Corregidos errores en validates_presence_of y en el fixture

--- app/models/family_record.rb
+++ app/models/family_record.rb
@@ -3,6 +3,6 @@ class FamilyRecord < ActiveRecord::Base
   belongs_to :patient
 
   validates_presence_of :pathology
-  validate_presence_of :relation_ship
+  validates_presence_of :relation_ship
 
 end
