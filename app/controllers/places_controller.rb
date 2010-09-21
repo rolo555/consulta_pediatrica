1661108c88de261b7d9d47440ad7c8e3048d9178
Jesús Mercado <jesusmmm@gmail.com>
2010-09-21 16:29:37 -0400

    Mejorado el close al abrir un popup de places.

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -5,14 +5,13 @@ class PlacesController < ApplicationController
 
   active_scaffold :place do |conf|
     conf.columns.exclude :patients
-    conf.create.action_after_create = 'close'
   end
 
-  def close()
+  def index
     if params[:must_close]
       render :template => "close", :layout => false
     else
-      return_to_main
+      list
     end
   end
 end
