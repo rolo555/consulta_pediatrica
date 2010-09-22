b95856ac70bbcb3f170ed10aea1282faf1682ca0
Jesús Mercado <jesusmmm@gmail.com>
2010-09-21 21:12:57 -0400

    En el bridge con paperclip ahora se utiliza tambien el plugin ligthbox

--- vendor/plugins/active_scaffold/lib/bridges/paperclip/lib/list_ui.rb
+++ vendor/plugins/active_scaffold/lib/bridges/paperclip/lib/list_ui.rb
@@ -3,15 +3,12 @@ module ActiveScaffold
     module ListColumnHelpers
       def active_scaffold_column_paperclip(column, record)
         paperclip = record.send("#{column.name}")
-        unless paperclip.file?
-          image_tag(paperclip.url, :border => 0)
-        end
-        content = if paperclip.styles.include?(PaperclipBridgeHelpers.thumbnail_style)
-          image_tag(paperclip.url(PaperclipBridgeHelpers.thumbnail_style), :border => 0)
+        thumbnail_path, original_path = if paperclip.file? and paperclip.styles.include?(PaperclipBridgeHelpers.thumbnail_style)
+          [paperclip.url(PaperclipBridgeHelpers.thumbnail_style), paperclip.url()]
         else
-          paperclip.original_filename
+          [paperclip.url, paperclip.url]
         end
-        link_to(content, paperclip.url, :popup => true)
+        lightbox_image_tag(thumbnail_path, original_path, {:class=>"images"})
       end
     end
   end
