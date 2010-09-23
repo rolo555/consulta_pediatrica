63b820cc44a0e27d9538f75d56dc0a59e9d42fbe
Jesús Mercado <jesusmmm@gmail.com>
2010-09-23 19:16:11 -0400

    Tests para consultation

--- app/models/consultation.rb
+++ app/models/consultation.rb
@@ -1,4 +1,30 @@
 class Consultation < ActiveRecord::Base
   #Relaciones
   belongs_to :patient
+
+  #Validaciones
+  validates_numericality_of :weight,
+    :only_integer => true,
+    :greater_than_or_equal_to => 0,
+    :allow_nil => true
+  validates_numericality_of :height,
+    :only_integer => true,
+    :greater_than_or_equal_to => 0,
+    :allow_nil => true
+  validates_numericality_of :temperature,
+    :only_integer => true,
+    :greater_than_or_equal_to => 0,
+    :allow_nil => true
+  validates_numericality_of :head_circumference,
+    :only_integer => true,
+    :greater_than_or_equal_to => 0,
+    :allow_nil => true
+  validates_numericality_of :anterior_fontanel,
+    :only_integer => true,
+    :greater_than_or_equal_to => 0,
+    :allow_nil => true
+  validates_numericality_of :amount,
+    :only_integer => true,
+    :greater_than_or_equal_to => 0,
+    :allow_nil => true
 end
--- test/unit/consultation_test.rb
+++ test/unit/consultation_test.rb
@@ -1,4 +1,30 @@
 require 'test_helper'
 
-class ConsultationTest <  Test::Unit::TestCase
+class ConsultationTest < ActiveSupport::TestCase
+#    t.float    "weight"
+#    t.float    "height"
+#    t.float    "head_circumference"
+#    t.float    "anterior_fontanel"
+#    t.float    "temperature"
+#    t.decimal  "amount"
+  neg = -1
+  should belong_to :patient
+  should validate_numericality_of :weight
+  should_not allow_value(neg).for(:weight)
+  should allow_value(nil).for(:weight)
+  should validate_numericality_of :height
+  should_not allow_value(neg).for(:height)
+  should allow_value(nil).for(:height)
+  should validate_numericality_of :head_circumference
+  should_not allow_value(neg).for(:head_circumference)
+  should allow_value(nil).for(:head_circumference)
+  should validate_numericality_of :anterior_fontanel
+  should_not allow_value(neg).for(:anterior_fontanel)
+  should allow_value(nil).for(:anterior_fontanel)
+  should validate_numericality_of :temperature
+  should_not allow_value(neg).for(:temperature)
+  should allow_value(nil).for(:temperature)
+  should validate_numericality_of :amount
+  should_not allow_value(neg).for(:amount)
+  should allow_value(nil).for(:amount)
 end
