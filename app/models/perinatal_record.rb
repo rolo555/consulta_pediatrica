f8d67ce901a6cffcf4411a81e25c7f762f6c2309
Jesús Mercado <jesusmmm@gmail.com>
2010-09-23 18:34:35 -0400

    Tests para perinatal_record

--- app/models/perinatal_record.rb
+++ app/models/perinatal_record.rb
@@ -1,4 +1,35 @@
 class PerinatalRecord < ActiveRecord::Base
   #Relaciones
   belongs_to :patient
+
+  #Validaciones
+  validates_numericality_of :number_of_pregnancy, 
+    :only_integer => true,
+    :greater_than_or_equal_to => 0
+  validates_numericality_of :childbirth,
+    :only_integer => true,
+    :greater_than_or_equal_to => 0
+  validates_numericality_of :cesarea,
+    :only_integer => true,
+    :greater_than_or_equal_to => 0
+  validates_numericality_of :abortions,
+    :only_integer => true,
+    :greater_than_or_equal_to => 0
+  validates_numericality_of :weeks_of_gestation,
+    :only_integer => true,
+    :greater_than_or_equal_to => 0
+  validates_numericality_of :apgar1,
+    :only_integer => true,
+    :greater_than_or_equal_to => 0
+  validates_numericality_of :apgar2,
+    :only_integer => true,
+    :greater_than_or_equal_to => 0
+  validates_numericality_of :weight,
+    :greater_than_or_equal_to => 0
+  validates_numericality_of :height,
+    :greater_than_or_equal_to => 0
+  validates_numericality_of :head_circumference,
+    :greater_than_or_equal_to => 0
+  validates_numericality_of :body_perimeter,
+    :greater_than_or_equal_to => 0
 end
