6c5e9ea631cac437c22a05fc2af622a9166acb68
Jesús Mercado <jesusmmm@gmail.com>
2010-09-30 18:29:07 -0400

    Agragados test to_label y tests de sanitizaciones para SurgicalRecord

--- app/models/surgical_record.rb
+++ app/models/surgical_record.rb
@@ -6,6 +6,10 @@ class SurgicalRecord < ActiveRecord::Base
   validates_presence_of :pathology, :procedure
   validates_length_of :pathology, :procedure, :maximum => 50
 
+  def to_label
+    "#{pathology} #{procedure}"
+  end
+
   protected
   def before_validation
     clean_whitespaces self.pathology, self.procedure
--- test/unit/surgical_record_test.rb
+++ test/unit/surgical_record_test.rb
@@ -6,4 +6,19 @@ class SurgicalRecordTest < ActiveSupport::TestCase
   should validate_presence_of :procedure
   should_not allow_value(@long_string).for(:pathology)
   should_not allow_value(@long_string).for(:procedure)
+
+  context "to_label" do
+    should "concat pathology and procedure" do
+      surgical_record = surgical_records(:one)
+      assert_equal(surgical_record.to_label, "pathology procedure");
+    end
+  end
+
+  should "sanitizate pathology" do
+    sanitizate("pathology")
+  end
+
+  should "sanitizate procedure" do
+    sanitizate("procedure")
+  end
 end
