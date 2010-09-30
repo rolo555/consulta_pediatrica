03aa4707756900d613773a59d7f84181ce43909b
Jesús Mercado <jesusmmm@gmail.com>
2010-09-30 17:56:22 -0400

    Agregadas pruebas para verificar la longitud máxima en los campos string de surgical_record

--- app/models/surgical_record.rb
+++ app/models/surgical_record.rb
@@ -2,9 +2,11 @@ class SurgicalRecord < ActiveRecord::Base
   #Relaciones
   belongs_to :patient
 
+  #Validaciones
   validates_presence_of :pathology, :procedure
-  validates_length_of :pathology, :procedure, :maximum => 50, :allow_nil => true
+  validates_length_of :pathology, :procedure, :maximum => 50
 
+  protected
   def before_validation
     clean_whitespaces self.pathology, self.procedure
   end
--- test/unit/surgical_record_test.rb
+++ test/unit/surgical_record_test.rb
@@ -4,4 +4,6 @@ class SurgicalRecordTest < ActiveSupport::TestCase
   should belong_to :patient
   should validate_presence_of :pathology
   should validate_presence_of :procedure
+  should_not allow_value(@long_string).for(:pathology)
+  should_not allow_value(@long_string).for(:procedure)
 end
