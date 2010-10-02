ac99cbffa9fdc19dbe1eccbd03200e7d1f03896e
rolo555 <rolo555@gmail.com>
2010-10-02 17:17:35 -0400

    test de pathological_records

--- test/unit/pathological_record_test.rb
+++ test/unit/pathological_record_test.rb
@@ -4,4 +4,15 @@ class PathologicalRecordTest < ActiveSupport::TestCase
   should belong_to :patient
   should validate_presence_of :diagnosis
   should validate_presence_of :treatment
+  should_not allow_value(@long_string).for(:diagnosis)
+  should_not allow_value(@long_string).for(:treatment)
+
+  should "sanitizate diagnosis" do
+    sanitizate "diagnosis"
+  end
+
+  should "sanitizate treatment" do
+    sanitizate "treatment"
+  end
+
 end
