379499e999c24d93117f0a74dd1870905efe279e
Jesús Mercado <jesusmmm@gmail.com>
2010-10-18 11:56:22 -0400

    Refactorizacion para que los autocompletados en cosultation esten al lado de sus campos

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,7 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
-  def text_area_and_link_to_record_select(record, field, options, controller)
-    text_area(record, field, options) + link_to_record_select("Pick an existing #{field.to_s.gsub '_', ' '}", controller.to_s, { :onselect => "", :html => { :class => 'autocomplete_field' }})
+  def text_area_and_link_to_record_select(field, options, controller)
+    column = active_scaffold_config.columns[field]
+    active_scaffold_input_textarea(column, options) + link_to_record_select("Pick an existing #{field.to_s.gsub '_', ' '}", controller.to_s, { :onselect => "", :html => { :class => 'autocomplete_field' }})
   end
 end
--- app/helpers/consultations_helper.rb
+++ app/helpers/consultations_helper.rb
@@ -1,17 +1,17 @@
 module ConsultationsHelper
   def consultation_laboratory_form_column(record, options)
-    text_area_and_link_to_record_select record, :laboratory, options, :laboratory_profiles
+    text_area_and_link_to_record_select :laboratory, options, :laboratory_profiles
   end
 
   def consultation_diagnosis_form_column(record, options)
-    text_area_and_link_to_record_select record, :diagnoses, options, :diagnostics
+    text_area_and_link_to_record_select :diagnosis, options, :diagnostics
   end
 
   def consultation_medical_certificate_form_column(record, options)
-    text_area_and_link_to_record_select record, :medical_certificate, options, :medical_certificates
+    text_area_and_link_to_record_select :medical_certificate, options, :medical_certificates
   end
 
   def consultation_order_form_column(record, options)
-    text_area_and_link_to_record_select record, :order, options, :medical_orders
+    text_area_and_link_to_record_select :order, options, :medical_orders
   end
 end
