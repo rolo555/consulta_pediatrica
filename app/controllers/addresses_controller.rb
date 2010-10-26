eb5aa1aee62840cab1bba2c1bc29b58859c9aa3a
Jesús Mercado <jesusmmm@gmail.com>
2010-10-26 18:35:44 -0400

    Comentarios al modelo address

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,3 +1,9 @@
 class AddressesController < ApplicationController
-  active_scaffold :address
+  active_scaffold :address do |conf|
+    #Excluir de todas las columnas la relación patient
+    conf.columns.exclude :patient
+
+    #Agregar a las columnas de listar la relación patient
+    conf.list.columns << :patient
+  end
 end
