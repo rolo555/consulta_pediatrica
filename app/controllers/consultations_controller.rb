3a2fb06cb43dd0197457e75b9ec1e161068ec2bd
rolo555 <rolo555@gmail.com>
2010-11-12 11:23:56 -0400

    print de consultation

--- app/controllers/consultations_controller.rb
+++ app/controllers/consultations_controller.rb
@@ -1,4 +1,32 @@
 class ConsultationsController < ApplicationController
+
+  def self.add_sub_groups (action)
+    action.columns.add_subgroup "personal_data" do |group|
+      group.add :weight, :height, :head_circumference, :anterior_fontanel, :temperature
+    end
+    action.columns.add_subgroup "current_condition" do |group|
+      group.add :current_condition
+    end
+    action.columns.add_subgroup "diagnosis" do |group|
+      group.add :diagnosis
+    end
+    action.columns.add_subgroup "medical_certificate" do |group|
+      group.add :medical_certificate
+    end
+    action.columns.add_subgroup "recipe" do |group|
+      group.add :recipe
+    end
+    action.columns.add_subgroup "order" do |group|
+      group.add :order
+    end
+    action.columns.add_subgroup "laboratory" do |group|
+      group.add :laboratory
+    end
+    action.columns.add_subgroup "amount" do |group|
+      group.add :amount
+    end
+  end
+
   active_scaffold :consultation do |conf|
     #Configuración de las columnas que se mostrarán
     conf.columns = [:weight, :height, :head_circumference, :anterior_fontanel,
@@ -12,6 +40,11 @@ class ConsultationsController < ApplicationController
     #Configuración de cómo se mostrará la columna amount
     conf.columns[:amount].options[:format] = :currency
     conf.columns[:amount].options[:i18n_options] = { :precision => 0 }
+
+    add_sub_groups conf.create
+    add_sub_groups conf.update
+    add_sub_groups conf.show
+
   end
 
   def default_values(consultation)
@@ -29,4 +62,16 @@ class ConsultationsController < ApplicationController
     @record = default_values(@record)
     @record
   end
+
+  def print
+    @consultation = Consultation.find params[:id]
+    respond_to do |format|
+      format.pdf do
+        render :pdf => "print",
+          :stylesheets => ["application","prince"],
+          :layout => "pdf"
+      end
+    end
+  end
+
 end
