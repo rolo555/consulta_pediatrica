e680e928bb75e310e5da24b05341619a682ddda9
Jesús Mercado <jesusmmm@gmail.com>
2010-11-19 04:30:52 -0400

    Mejorado el helper de patient

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,9 @@ module ApplicationHelper
     column = active_scaffold_config.columns[field]
     active_scaffold_input_textarea(column, options) + link_to_record_select(image_tag("active_scaffold/default/magnifier.png")+" Find", controller.to_s, { :onselect => "", :html => { :class => 'autocomplete_field' }})
   end
+
+  def plural_for(model)
+    #as_("#{model}.other", :scope => "activerecord.models")
+    active_scaffold_config_for(model).list.label
+  end
 end
--- app/helpers/patients_helper.rb
+++ app/helpers/patients_helper.rb
@@ -1,27 +1,34 @@
 module PatientsHelper
   def patient_consultations_column(record)
-    image_tag "icons/consultation.jpg", :title => "Consultations"
+    image_tag "icons/consultation.jpg", :title => plural_for(Consultation)
   end
+
   def patient_family_records_column(record)
-    image_tag "icons/familyRecord.gif", :title => "Family Records"
+    image_tag "icons/familyRecord.gif", :title => plural_for(FamilyRecord)
   end
+
   def patient_allergies_column(record)
     if record.allergies.empty?
-      image_tag "icons/noAllergy.png", :title => "Allergies"
+      icon = "icons/noAllergy.png"
     else
-      image_tag "icons/allergy.png", :title => "Allergies"
+      icon = "icons/allergy.png"
     end
+    image_tag icon, :title => plural_for(Allergy)
   end
+
   def patient_perinatal_record_column(record)
-    image_tag "icons/perinatalRecord.png", :title => "Perinatal Records"
+    image_tag "icons/perinatalRecord.png", :title => plural_for(PerinatalRecord)
   end
+
   def patient_surgical_records_column(record)
-    image_tag "icons/surgicalRecord.png", :title => "Surgical Records"
+    image_tag "icons/surgicalRecord.png", :title => plural_for(SurgicalRecord)
   end
+
   def patient_pathological_records_column(record)
-    image_tag "icons/pathologicalRecord.png", :title => "Pathological Records"
+    image_tag "icons/pathologicalRecord.png", :title => plural_for(PathologicalRecord)
   end
+
   def patient_surgeries_column(record)
-    image_tag "icons/surgery.png", :title => "Surgeries"
+    image_tag "icons/surgery.png", :title => plural_for(Surgery)
   end
 end
