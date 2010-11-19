c4ef8f25be7d0438f04bf5039b336d4b846f338a
rolo555 <rolo555@gmail.com>
2010-11-19 01:26:46 -0400

    correccion del activescaffold para que funcionen las traducciones

--- vendor/plugins/active_scaffold/lib/extensions/localize.rb
+++ vendor/plugins/active_scaffold/lib/extensions/localize.rb
@@ -1,8 +1,9 @@
 class Object
   def as_(key, options = {})
     unless key.blank?
-      text = I18n.translate "%{key}", {:scope => [:active_scaffold], :default => key.is_a?(String) ? key : key.to_s.titleize}.merge(options)
-      # text = nil if text.include?('translation missing:')
+      text = I18n.translate "#{key}", {:scope => [:active_scaffold]}.merge(options)
+      #text = I18n.translate "%{key}", {:scope => [:active_scaffold], :default => key.is_a?(String) ? key : key.to_s.titleize}.merge(options)
+      #text = nil if text.include?('translation missing:')
     end
     text ||= key 
     text
