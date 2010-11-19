b4f3ea5a668e7ebf582736a2f549122add3fe7f6
Jesús Mercado <jesusmmm@gmail.com>
2010-11-19 03:58:31 -0400

    Mejorar patient

--- app/controllers/consultation_prices_controller.rb
+++ app/controllers/consultation_prices_controller.rb
@@ -1,4 +1,8 @@
 class ConsultationPricesController < ApplicationController
+  record_select :per_page => 5,
+    :search_on => ['price_type', 'amount'],
+    :full_text_search => true
+
   active_scaffold :consultation_price do |conf|
     #Configuración de las columnas que se mostrarán
     conf.columns = [:default, :price_type, :amount]
--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,5 +1,4 @@
 class PatientsController < ApplicationController
-
   def self.add_sub_groups (action)
     action.columns.add_subgroup "Personal data" do |group|
       group.add :photograph, :first_name, :last_name, :date_of_birth, :place
@@ -17,58 +16,53 @@ class PatientsController < ApplicationController
   end
 
   active_scaffold :patient do |conf|
-    #Configuración de las columnas que se mostrarán al listar
+    #Columnas que se mostrarán al listar
     conf.list.columns = :photograph, :last_name, :first_name, :age, 
       :consultations, :family_records, :allergies, :perinatal_record,
       :surgical_records, :pathological_records, :surgeries
 
-    #Configuración de las columnas que se excluiran para todas las acciones
+    #Columnas que se excluiran para todas las acciones
     conf.columns.exclude :photograph_content_type, :photograph_file_name,
       :photograph_file_size, :photograph_updated_at, :family_records,
       :allergies, :perinatal_record, :surgical_records, :pathological_records,
       :consultations, :surgeries
 
-    #Configuración del boton generar historial
+    #Botón generar historial
     conf.action_links.add :patient_history,
       :type => :member,
       :page => true
 
-    #Configuración del boton clonar
+    #Botón clonar
     conf.action_links.add :clone,
       :type => :member,
       :confirm => "Are you sure to clone patient?",
       :parameters => { :controller => 'patients', :action => 'new' }
 
-    #Configuración de los agrupados por categorias para la acción create
-    add_sub_groups conf.create
-    add_sub_groups conf.update
-    add_sub_groups conf.show
-
-    #Configuracion de [:date_of_birth] para que muestre mas años
+    #:date_of_birth muestra muestre mas años
     conf.columns[:date_of_birth].options = {:end_year => Date.today.year-30, :start_year => Date.today.year, :include_blank => false }
 
+    #Eliminar las etiquetas al listar de todas las relaciones
     conf.columns[:consultations].label = ""
-
     conf.columns[:surgical_records].label = ""
-
     conf.columns[:pathological_records].label = ""
-
     conf.columns[:family_records].label = ""
-
     conf.columns[:allergies].label = ""
-
     conf.columns[:perinatal_record].label = ""
-
-    conf.columns[:consultation_price].form_ui = :select
-
+    conf.columns[:surgeries].label = ""
+    
+    #Algunos campos se muestran con :record_select
+    conf.columns[:consultation_price].form_ui = :record_select
     conf.columns[:place].form_ui = :record_select
 
-    conf.columns[:surgeries].label = ""
-
-    #Configuracion de :perinatal_record para que sea un nested action
+    #:perinatal_record es un nested action
     conf.columns[:perinatal_record].set_link 'nested',
       :controller =>'patients',
       :parameters => { :associations => :perinatal_record }
+
+    #agrupar por categorias cada acción
+    add_sub_groups conf.create
+    add_sub_groups conf.update
+    add_sub_groups conf.show
   end
 
   def do_new
