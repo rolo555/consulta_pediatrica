b6c2bcef91ecfe67b527456e2e75e7fdfda945b4
Jesús Mercado <jesusmmm@gmail.com>
2010-11-22 11:19:44 -0400

    Cuando drug valida sus campos enteros, ahora activescaffold no limpia el campo al validar

--- app/controllers/drugs_controller.rb
+++ app/controllers/drugs_controller.rb
@@ -6,5 +6,7 @@ class DrugsController < ApplicationController
 
   active_scaffold :drugs do |conf|
     conf.columns = [:name, :how_often_in_hours, :how_often_in_weight, :recipe]
+    conf.columns[:how_often_in_hours].options[:format] = nil
+    conf.columns[:how_often_in_weight].options[:format] = nil
   end
 end
