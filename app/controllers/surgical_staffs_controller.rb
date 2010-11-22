1f4c5cabc2c2b67f10579bcbcbef3f3add64bdcc
Jesús Mercado <jesusmmm@gmail.com>
2010-11-22 12:03:34 -0400

    Eliminar percetage de surgical_staff

--- app/controllers/surgical_staffs_controller.rb
+++ app/controllers/surgical_staffs_controller.rb
@@ -4,9 +4,6 @@ class SurgicalStaffsController < ApplicationController
     :full_text_search => true
 
   active_scaffold :surgical_staffs do |conf|
-    conf.columns = :is_always_present, :staff, :percentage
-    
-    conf.columns[:percentage].options[:format] = :percentage
-    conf.columns[:percentage].options[:i18n_options] = { :precision => 1 }
+    conf.columns = :is_always_present, :staff
   end
 end
--- app/models/surgical_staff.rb
+++ app/models/surgical_staff.rb
@@ -1,12 +1,10 @@
 include ModelHelper
 
 class SurgicalStaff < ActiveRecord::Base
-
   has_and_belongs_to_many :surgeries
 
-  validates_presence_of :staff, :percentage
-  validates_numericality_of :percentage, :grater_than => 0, :less_than_or_equal_to => 100
-  validates_length_of :staff, :maximum => 50, :if => "self.staff.presence"
+  validates_presence_of :staff
+  validates_length_of :staff, :maximum => 50
   validates_uniqueness_of :staff, :case_sensitive => false
 
   def before_validation
@@ -14,7 +12,6 @@ class SurgicalStaff < ActiveRecord::Base
   end
 
   def to_label
-    "#{staff} #{percentage}"
+    "#{staff}"
   end
-
 end
