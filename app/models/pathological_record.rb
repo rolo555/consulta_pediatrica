86de9509590272c42666502e8888a2eea853a665
Jesús Mercado <jesusmmm@gmail.com>
2010-11-23 21:24:53 -0400

    Correcciones menores a pathological record

--- app/models/pathological_record.rb
+++ app/models/pathological_record.rb
@@ -1,7 +1,6 @@
 include ModelHelper
 
 class PathologicalRecord < ActiveRecord::Base
-
   protected :before_validation
 
   #Relations
@@ -10,13 +9,13 @@ class PathologicalRecord < ActiveRecord::Base
   validates_presence_of :diagnosis, :treatment
   validates_length_of :diagnosis, :maximum => 50, :if => "self.diagnosis.presence"
   validates_length_of :treatment, :maximum => 50, :if => "self.treatment.presence"
-  validate :date_must_be_lower_than_tomorrow
+  validate :date_cant_be_greater_than_today
 
   def before_validation
     sanitizate_strings :diagnosis, :treatment
   end
 
-  def date_must_be_lower_than_tomorrow
+  def date_cant_be_greater_than_today
     unless self.date.nil?
       if (self.date <=> Date.today) > 0
         errors.add :date, 'can\'t be greater than today'
