3033d69cfc509818a6793603e3a6cde20ac68ae2
Jesús Mercado <jesusmmm@gmail.com>
2010-11-26 16:20:40 -0400

    Agregar helper para que al ver un surgery_quotation se muestren los hospital_expenses en una lista

--- app/helpers/surgery_quotations_helper.rb
+++ app/helpers/surgery_quotations_helper.rb
@@ -2,4 +2,9 @@ module SurgeryQuotationsHelper
   def surgery_quotation_total_expenses_column(record)
     record.total_expenses
   end
+
+  def surgery_quotation_hospital_expenses_show_column(record)
+    list = record.hospital_expenses.map { |item| content_tag "li", item.to_label }
+    content_tag "ul", list
+  end
 end
