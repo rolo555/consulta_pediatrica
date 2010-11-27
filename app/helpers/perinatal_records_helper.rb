d8b29b1b8c42f01d76e4a2a926f2d7d83098b604
Jesús Mercado <jesusmmm@gmail.com>
2010-11-27 14:11:37 -0400

    Mejoras a los helpers de perinatal records

--- app/helpers/perinatal_records_helper.rb
+++ app/helpers/perinatal_records_helper.rb
@@ -1,2 +1,33 @@
 module PerinatalRecordsHelper
+  def perinatal_record_weight_column(record)
+    unless record.weight.blank?
+      "#{record.weight} #{I18n.t :grams, :scope => [:number, :weight]}"
+    else
+      "-"
+    end
+  end
+
+  def perinatal_record_height_column(record)
+    unless record.height.blank?
+      "#{record.height} #{I18n.t :centimeters, :scope => [:number, :height]}"
+    else
+      "-"
+    end
+  end
+
+  def perinatal_record_head_circumference_column(record)
+    unless record.head_circumference.blank?
+    "#{record.head_circumference} #{I18n.t :grams, :scope => [:number, :weight]}"
+    else
+      "-"
+    end
+  end
+
+  def perinatal_record_body_perimeter_column(record)
+    unless record.head_circumference.blank?
+      "#{record.body_perimeter} #{I18n.t :centimeters, :scope => [:number, :height]}" unless record.body_perimeter.blank?
+    else
+      "-"
+    end
+  end
 end
