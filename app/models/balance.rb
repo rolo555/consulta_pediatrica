f10bbfb30b9e54fcb256a67753c0693a2d8f64fe
Jesús Mercado <jesusmmm@gmail.com>
2010-12-03 19:00:54 -0400

    Mejorar el código de las validaciones de balance

--- app/models/balance.rb
+++ app/models/balance.rb
@@ -1,12 +1,12 @@
 class Balance < ActiveRecord::Base
-
+  #Validaciones
   validates_presence_of :date_start, :date_finish
   validate :date_start_must_be_lower_than_date_finish
   validate :dates_must_be_lower_than_tomorrow
 
   def date_start_must_be_lower_than_date_finish
     unless self.date_start.nil? or self.date_finish.nil?
-      if (self.date_start <=> self.date_finish ) > 0
+      if self.date_start > self.date_finish
         errors.add :date_start, "#{as_('can\'t be greater than')} #{as_('date_finish', :scope => 'activerecord.attributes.balance' )}"
       end
     end
@@ -14,13 +14,12 @@ class Balance < ActiveRecord::Base
 
   def dates_must_be_lower_than_tomorrow
     unless self.date_start.nil? or self.date_finish.nil?
-      if (self.date_start <=> Date.today ) > 0
+      if self.date_start > Date.today
         errors.add :date_start, "#{as_('can\'t be greater than')} #{as_('today')}"
       end
-      if (self.date_finish <=> Date.today ) > 0
+      if self.date_finish > Date.today
         errors.add :date_finish, "#{as_('can\'t be greater than')} #{as_('today')}"
       end
     end
   end
-
 end
