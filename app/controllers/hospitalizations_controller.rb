5b5797bd95a68b6fcf8b62571a0bd3ac2d135044
rolo555 <rolo555@gmail.com>
2010-12-14 18:06:58 -0400

    link para imprimir hospitalizacion (falta buscar la informacion)

--- app/controllers/hospitalizations_controller.rb
+++ app/controllers/hospitalizations_controller.rb
@@ -2,5 +2,19 @@ class HospitalizationsController < ApplicationController
   active_scaffold :hospitalization do |conf|
     conf.list.columns = :created_at, :doctors_names, :room, :hospitalization_follow_up
     conf.columns.exclude :hospitalization_follow_up
+    #Link a imprimir
+    conf.action_links.add :print,
+      :type => :member,
+      :page => true
+  end
+
+  def print
+    respond_to do |format|
+      format.pdf do
+        render :pdf => "print",
+          :stylesheets => ["application","prince"],
+          :layout => "pdf"
+      end
+    end
   end
 end
