02011edebfb50f98af6fa63a61d49d19b252717a
Jesús Mercado <jesusmmm@gmail.com>
2010-12-15 19:01:31 -0400

    Configurar AS
    Mostrar application_type en un selectbox con las opciones: dosis y refuerzo

--- app/controllers/vaccine_schedules_controller.rb
+++ app/controllers/vaccine_schedules_controller.rb
@@ -1,3 +1,6 @@
 class VaccineSchedulesController < ApplicationController
-  active_scaffold :vaccine_schedule
+  active_scaffold :vaccine_schedule do |conf|
+    conf.columns[:application_type].form_ui = :select
+    conf.columns[:application_type].options = {:options => [:dosis, :refuerzo], :include_blank => true}
+  end
 end
