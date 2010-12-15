917b873c5f1408ac67e178b4a8b77dc2d63a31de
Jesús Mercado <jesusmmm@gmail.com>
2010-12-15 19:04:00 -0400

    Configurar AS
    Mostrar vaccine_schedule dentro de vaccines_name

--- app/controllers/vaccines_names_controller.rb
+++ app/controllers/vaccines_names_controller.rb
@@ -4,7 +4,8 @@ class VaccinesNamesController < ApplicationController
     :full_text_search => true
 
   active_scaffold :vaccines_name do |conf|
-    conf.columns = [:name]
+    conf.columns = :name, :vaccine_schedule
+    conf.list.columns.exclude :vaccine_schedule
   end
 
   def index
