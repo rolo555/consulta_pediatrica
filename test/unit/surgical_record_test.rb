deb42f5252e208fca14b96b21e15778e46b3f76c
Jesús Mercado <jesusmmm@gmail.com>
2010-12-30 18:14:00 -0400

    Corregir prueba de surgical_record, el maximo de pathology es 100

--- test/unit/surgical_record_test.rb
+++ test/unit/surgical_record_test.rb
@@ -4,7 +4,7 @@ class SurgicalRecordTest < ActiveSupport::TestCase
   should belong_to :patient
   should validate_presence_of :pathology
   should validate_presence_of :procedure
-  should ensure_length_of(:pathology).is_at_most(50)
+  should ensure_length_of(:pathology).is_at_most(100)
   should validate_uniqueness_of(:pathology).case_insensitive.scoped_to(:patient_id)
 #  should_not allow_value(Date.tomorrow).for(:date)
   should_not allow_value(Date.today.year+1).for(:year)
