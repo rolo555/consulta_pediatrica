a67d3de4fcba22a479c2b6a8cff5bfd6990e6aff
Jesús Mercado <jesusmmm@gmail.com>
2011-01-07 16:58:11 -0400

    Refactorizar las pruebas de Consultation

--- test/unit/consultation_test.rb
+++ test/unit/consultation_test.rb
@@ -1,29 +1,22 @@
 require 'test_helper'
 
 class ConsultationTest < ActiveSupport::TestCase
-  neg = -1
   should belong_to :patient
   should have_many(:images)#.dependent(:destroy)
   should have_many(:surgery_quotations)#.dependent(:destroy)
   should have_one :income
-  should validate_numericality_of :weight
-  should_not allow_value(neg).for(:weight)
-  should allow_value(nil).for(:weight)
-  should validate_numericality_of :height
-  should_not allow_value(neg).for(:height)
-  should allow_value(nil).for(:height)
-  should validate_numericality_of :head_circumference
-  should_not allow_value(neg).for(:head_circumference)
-  should allow_value(nil).for(:head_circumference)
-  should validate_numericality_of :anterior_fontanel
-  should_not allow_value(neg).for(:anterior_fontanel)
-  should allow_value(nil).for(:anterior_fontanel)
-  should validate_numericality_of :temperature
-  should_not allow_value(neg).for(:temperature)
-  should allow_value(nil).for(:temperature)
-  should validate_numericality_of :amount
-  should_not allow_value(neg).for(:amount)
-  should allow_value(nil).for(:amount)
+
+  [:weight, :height, :head_circumference, :temperature, :amount].each do |field|
+    should validate_numericality_of field
+  end
+
+  [:weight, :height, :head_circumference, :temperature, :amount].each do |field|
+    should_not allow_value(-1).for field
+  end
+
+  [:weight, :height, :head_circumference, :temperature, :amount, :anterior_fontanel].each do |field|
+    should allow_value(nil).for field
+  end
   
   context "A new instance of consultation" do
     should "copy patient.amount to amount if patient exists" do
