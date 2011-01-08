3c10a5a3b5c1fcfca3bbb2f354bbd775ca087f70
Jesús Mercado <jesusmmm@gmail.com>
2011-01-08 19:06:04 -0400

    Quitar código comentado

--- app/controllers/consultations_controller.rb
+++ app/controllers/consultations_controller.rb
@@ -1,32 +1,4 @@
 class ConsultationsController < ApplicationController
-
-  def self.add_sub_groups (action)
-    action.columns.add_subgroup "personal_data" do |group|
-      group.add :weight, :height, :head_circumference, :anterior_fontanel, :temperature
-    end
-    action.columns.add_subgroup "current_condition" do |group|
-      group.add :current_condition
-    end
-    action.columns.add_subgroup "recipe" do |group|
-      group.add :recipe
-    end
-    action.columns.add_subgroup "diagnosis" do |group|
-      group.add :diagnosis
-    end
-    action.columns.add_subgroup "medical_certificate" do |group|
-      group.add :medical_certificate
-    end
-    action.columns.add_subgroup "order" do |group|
-      group.add :order
-    end
-    action.columns.add_subgroup "laboratory" do |group|
-      group.add :laboratory
-    end
-    action.columns.add_subgroup "amount" do |group|
-      group.add :amount
-    end
-  end
-
   active_scaffold :consultation do |conf|
     #Configuración de las columnas que se mostrarán
     conf.columns = [:weight, :height, :head_circumference, :anterior_fontanel,
@@ -41,13 +13,8 @@ class ConsultationsController < ApplicationController
     conf.columns[:amount].options[:format] = :currency
     conf.columns[:amount].options[:i18n_options] = { :precision => 0 }
 
-#    add_sub_groups conf.create
-#    add_sub_groups conf.update
-    #add_sub_groups conf.show
-
     conf.action_links.add :print,
       :type => :member
-    #      :parameters => { :id => "0" }
   end
 
   def do_new
@@ -71,16 +38,6 @@ class ConsultationsController < ApplicationController
       render :layout => false
     end
   end
-  #  def print
-  #    @consultation = Consultation.find params[:id]
-  #    respond_to do |format|
-  #      format.pdf do
-  #        render :pdf => "print",
-  #          :stylesheets => ["application","prince"],
-  #          :layout => "pdf"
-  #      end
-  #    end
-  #  end
 
   def update_drugs
     @drugs = Set.new
