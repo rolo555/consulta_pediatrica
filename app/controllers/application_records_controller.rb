68bf3f8c5434f681963d4abf3211e89e2c57d239
Jesús Mercado <jesusmmm@gmail.com>
2011-01-10 18:46:33 -0400

    Cambiar vaccine por un select

--- app/controllers/application_records_controller.rb
+++ app/controllers/application_records_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationRecordsController < ApplicationController
   active_scaffold :application_record do |conf|
     conf.columns = [:application_type, :date, :doctor_application, :vaccine]
+    conf.columns[:vaccine].form_ui = :select
     conf.columns[:application_type].form_ui = :select
     conf.columns[:application_type].options = {:options => [:dosis, :refuerzo], :include_blank => true}
     conf.columns[:date].options = {:end_year => Date.today.year-30, :start_year => Date.today.year, :include_blank => true}
