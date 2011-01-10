57bfccdf74a6bd366cc7a57713339a2acf4ca880
Jesús Mercado <jesusmmm@gmail.com>
2011-01-10 18:48:28 -0400

    Filtrar las vacunas a seleccionar

--- app/helpers/application_records_helper.rb
+++ app/helpers/application_records_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationRecordsHelper
+  def options_for_association_conditions(association)
+    if association.name == :vaccine
+      { 'vaccines.vaccines_name_id' => @record.immunization_record.vaccines_name_id }
+    else
+      super
+    end
+  end
 end
