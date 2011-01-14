e7c763f3c577ce2ee83e70555dedb42ef83c8262
Jesús Mercado <jesusmmm@gmail.com>
2011-01-14 18:50:33 -0400

    Ocultar vaccine si doctor_application esta en false

--- app/controllers/application_records_controller.rb
+++ app/controllers/application_records_controller.rb
@@ -1,9 +1,16 @@
 class ApplicationRecordsController < ApplicationController
   active_scaffold :application_record do |conf|
     conf.columns = [:application_type, :date, :doctor_application, :vaccine]
+    conf.list.columns.exclude :vaccine
     conf.columns[:vaccine].form_ui = :select
     conf.columns[:application_type].form_ui = :select
     conf.columns[:application_type].options = {:options => [:dosis, :refuerzo], :include_blank => true}
     conf.columns[:date].options = {:end_year => Date.today.year-30, :start_year => Date.today.year, :include_blank => true}
   end
+
+#  def after_render_field(record, column)
+#    if column.name == :doctor_application
+#      record.doctor_application = params[:record][:doctor_application]
+#    end
+#  end
 end
--- app/helpers/application_records_helper.rb
+++ app/helpers/application_records_helper.rb
@@ -6,4 +6,25 @@ module ApplicationRecordsHelper
       super
     end
   end
+
+  def application_record_doctor_application_form_column(record, options)
+    js = "var vaccines = this.up('.form-element').next(); if(this.checked) vaccines.show(); else vaccines.hide();"
+    options[:onchange] = js
+    column = active_scaffold_config.columns[:doctor_application]
+    active_scaffold_input_checkbox column, options
+  end
+
+  #  def application_record_vaccine_form_column(record, html_options)
+  #    if record.doctor_application
+  #      debugger
+  #      column = active_scaffold_config.columns[:vaccine]
+  #      options = { :selected => @record.send(column.name).to_s }
+  #      options_for_select = active_scaffold_translated_options(column)
+  #      html_options.update(column.options[:html_options] || {})
+  #      options.update(column.options)
+  #      select(:record, column.name, options_for_select, options, html_options)
+  #    else
+  #      content_tag "span", "", html_options
+  #    end
+  #  end
 end
